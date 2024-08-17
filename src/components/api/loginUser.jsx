d2dcd930a05fa954034577b610a0a8947ddf7324
rakanugraha18 <rakanugraha2@gmail.com>
2024-08-17 11:42:32 +0700

    Membuat halaman login dan register untuk admin

---
+++ src/components/api/loginUser.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_BASEURL;
+
+const loginUser = async (email, password) => {
+  try {
+    const resLogin = await axios.post(
+      `${API_URL}/admin/login-admin`,
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const token = resLogin.data.token;
+    const user_id = resLogin.data.userId;
+    localStorage.setItem("token", token);
+    localStorage.setItem("user_id", user_id);
+
+    return token;
+  } catch (error) {
+    console.error("Login failed", error);
+    throw error;
+  }
+};
+
+export default loginUser;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import DashboardPage from "./pages/dashboard.jsx";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <DashboardPage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/register", element: <RegisterPage /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
